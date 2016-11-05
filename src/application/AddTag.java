fe7d0d39b87a8ed3f482be695124b4e0db91ae9d
Mike Tomkowich <mike.tomkowich@Mikes-Air.home>
2016-11-04 21:38:23 -0400

    Add tag skeleton

---
+++ src/application/AddTag.java
@@ -0,0 +1,34 @@
+package application;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+public class AddTag extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		System.out.println("poop");
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("/application/AddTag.fxml"));
+		
+		AnchorPane root = (AnchorPane)loader.load();
+		
+		AddTagController AddTagController=loader.getController();
+		//loginController.start(primaryStage);
+		
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("Login Page");
+		primaryStage.setScene(scene);
+		System.out.println("poop");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/AddTagController.java
@@ -0,0 +1,34 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddTagController {
+
+    @FXML
+    private ComboBox<?> tagDropDown;
+
+    @FXML
+    private TextField tagText;
+
+    @FXML
+    private Button addTag;
+
+    @FXML
+    private Button createNewTagType;
+    
+    public void handle(ActionEvent e){
+    	Button b = (Button)e.getSource();
+    	if(b == addTag){
+    		System.out.println("add Tag");
+    	}
+    	else if(b == createNewTagType){
+    		System.out.println("Create new tag type");
+    		
+    	}
+    }
+
+}
