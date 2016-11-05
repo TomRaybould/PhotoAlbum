9110edd77b8dc2edaa2fc151d852dac29baaf5b3
Mike Tomkowich <mike.tomkowich@Mikes-Air.home>
2016-11-04 22:03:32 -0400

    SR skeleton

---
+++ src/application/SearchResultsController.java
@@ -0,0 +1,83 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SearchResultsController {
+
+    @FXML
+    private Button displayPicture;
+
+    @FXML
+    private Button slideShow;
+
+    @FXML
+    private Button backToAlbum;
+
+    @FXML
+    private Button removePhoto;
+
+    @FXML
+    private Button movePhoto;
+
+    @FXML
+    private Button copyPhoto;
+
+    @FXML
+    private Button addEditCaption;
+
+    @FXML
+    private Button addTag;
+
+    @FXML
+    private Button removeTag;
+
+    @FXML
+    private Button createAlbum;
+    
+    public void handle(ActionEvent e){
+    	Button b = (Button)e.getSource();
+    	if(b == displayPicture){
+    		System.out.println("add Tag");
+    	}
+    	else if(b == slideShow){
+    		System.out.println("slideShow");
+    		
+    	}
+    	else if(b == backToAlbum){
+    		System.out.println("back to album");
+    		
+    	}
+    	else if(b == removePhoto){
+    		System.out.println("remove photo");
+    		
+    	}
+    	else if(b == movePhoto){
+    		System.out.println("move photo");
+    		
+    	}
+    	else if(b == copyPhoto){
+    		System.out.println("copy photo");
+    		
+    	}
+    	else if(b == addEditCaption){
+    		System.out.println("add edit caption");
+    		
+    	}
+    	else if(b == addTag){
+    		System.out.println("add tag");
+    		
+    	}
+    	else if(b == removeTag){
+    		System.out.println("remove tag");
+    		
+    	}
+    	else if(b == createAlbum){
+    		System.out.println("Create Album");
+    		
+    	}
+    }
+
+
+}
